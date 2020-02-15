18197789bf09fa5783e1b4067696334874f464b7
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-02-15 13:10:50 -0500

    1

---
+++ config.py
@@ -0,0 +1,11 @@
+
+login_page = 'https://secure.indeed.com/account/login?hl=en_US&co=US&continue=https%3A%2F%2Fwww.indeed.com%2F&tmpl=desktop&service=my&from=gnav-util-homepage' # login page 
+
+username = 'blahblabh@gmail.com ' # put your username here
+password = 'password12345' # put your passwordhere
+
+job_search = '' #job search url
+job_urls = [] #job urls
+
+hello= ''
+testing = ' '
