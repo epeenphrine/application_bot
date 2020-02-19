2415774a61e3a7472cbf7cdae3420caecc0461ba
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-02-19 18:16:04 -0500

    building iframe handler to input information

--- app_bot.py
+++ app_bot.py
@@ -7,8 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 ## standard library imports 
 import re
 import time 
+import json
 ## non standard and non pip install imports 
 from config2 import username, password, job_search, job_urls, login_page
+
+
+#EC.text_to_be_present_in_element((By.ID, "operations_monitoring_tab_current_ct_fields_no_data"), "No data to display")
 
 class AppBot:
     def __init__(self):
@@ -42,6 +46,7 @@ class AppBot:
         ## press ENTER key 
         select_password.send_keys(Keys.ENTER)
         self.driver.switch_to_window(self.driver.window_handles[0])
+
     # stores href of all job posting in a list
     def find_jobs(self):
         self.driver.get(self.initial_job[0])
@@ -54,64 +59,90 @@ class AppBot:
                     self.hrefs.append(href)
                     print(href)
             self.driver.find_element_by_class_name('np').click()
-            time.sleep(2)
+            ## popup that may come up when pressing next page 
+            wait = WebDriverWait(self.driver, 2)
             try:
-                ## popup that may come up when pressing next page 
-                self.driver.find_element_by_xpath('//*[@id="popover-close-link"]').click()
+                popup = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="popover-close-link"]')))
+                popup.click()
             except:
-                pass        
+                print('no popup')
             print(len(self.hrefs))
 
     def iframe_handler(self):
-        wait = WebDriverWait(self.driver, 10)
+        wait = WebDriverWait(self.driver, 2)
+        try:
+            frame = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe[name$=modal-iframe]')))
+            self.driver.switch_to.frame(frame)
+            time.sleep(2)
+            self.driver.switch_to.frame(0)
+        except:
+            frame = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="indeedapply-modal-preload-iframe"]')))
+            self.driver.switch_to.frame(frame)
+            time.sleep(2)
+            self.driver.switch_to.frame(0)
+        textareas = self.driver.find_elements_by_xpath('//*[starts-with(@id, "q_")]/div')
+        print(textareas)
+        textinputs = []
+        for item in textareas:
+            text_item = item.text
+            if text_item:
+                print(text_item)
+                textinputs.append(item)
+
+    def iframe_closer(self):
+        wait = WebDriverWait(self.driver, 2)
         frame = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[name$=modal-iframe]")))
         self.driver.switch_to.frame(frame)
         time.sleep(2)
         self.driver.switch_to.frame(0)
         try:
-            self.driver.find_element_by_id('form-action-cancel').click()
-            self.driver.implicitly_wait(5)
+            wait = WebDriverWait(self.driver, 2)
+            cancel_button = wait.until(EC.presence_of_element_located((By.ID, '//*[@id="popover-close-link"]')))
+            cancel_button.click()
         except:
             pass
-        try:
-            self.driver.find_element_by_xpath('//*[@id="close-popup"]').click()
+        try: 
+            wait = WebDriverWait(self.driver, 2)
+            cancel_button = wait.until(EC.presence_of_element_located((By.ID, '//*[@id="close-popup"]')))
+            cancel_button.click()
         except:
             pass
 
     def click_job(self):
         #jobs = self.driver.find_elements_by_class_name("jobsearch-SerpJobCard")
-        wait = WebDriverWait(self.driver, 10)
-        apply_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="indeedApplyButtonContainer"]/span/div[2]/button')))
-        #jobs = self.driver.find_elements_by_class_name("jobsearch-SerpJobCard") 
-        print(apply_button)
-        apply_button.click()
-        #for job in jobs:
-            #job.click()
-            #print(f'clicked {job}')
-            #try:
-                #self.driver.find_element_by_class_name('indeed-apply-button').click()
-                #wait = WebDriverWait(self.driver, 10)
-                #frame = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[name$=modal-iframe]")))
-                #self.driver.switch_to.frame(frame)
-                #time.sleep(2)
-                #self.driver.switch_to.frame(0)
-                #self.driver.find_element_by_id('form-action-cancel').click()
-                #self.driver.implicitly_wait(5)
-            #except:
-               #print("company site")
+        self.driver.get('https://www.indeed.com/viewjob?from=serp,iaBackPress,iaBackPress,iaBackPress&jk=3f2f91cc5ac41d8e&vjs=3')
+        try:
+            print('trying first method')
+            wait = WebDriverWait(self.driver, 2)
+            apply_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="indeedApplyButtonContainer"]/span/div[2]/button')))
+            #jobs = self.driver.find_elements_by_class_name("jobsearch-SerpJobCard") 
+            print(apply_button)
+            apply_button.click()
+        except:
+            print('trying second method')
+            wait = WebDriverWait(self.driver, 2)
+            apply_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="indeedApplyButtonContainer"]/span/div[1]/button')))
+            print(apply_button)
+            apply_button.click()
+      
     def apply_jobs(self):
+        clickable_jobs = []
         for href in self.hrefs:
             print(f" trying : {href} ")
             self.driver.get(href)
             try: 
                 self.click_job()
-                print('clicked jobs')
-                time.sleep(2)
+                print('\nclicked jobs')
+                clickable_jobs.append(href)
+                self.iframe_closer()
+                print('\niframe handled')
+                time.sleep(1)
             except:
                 print('didnt work or takes to company site')
+        with open ('clickable_jobs.json', 'w') as f:
+            json.dump(clickable_jobs, f)
 bot = AppBot()
 bot.login()
 time.sleep(4)
-bot.find_jobs()
-time.sleep(4)
-bot.apply_jobs()
+bot.click_job()
+bot.iframe_handler()
