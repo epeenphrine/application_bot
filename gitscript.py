3b5f8a7b3aabf77f1c4fb82da88453ad846cfe2e
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-04-01 02:39:39 -0400

    testing

--- gitscript.py
+++
@@ -1,7 +0,0 @@
-import os
-
-commit_message = input('commit mesage: ')
-
-os.system(f"git add . && git reset config.py && git status && git commit -m '{commit_message}' ")
-
-print('commit made')
--- gitscript.sh
+++
@@ -1,2 +0,0 @@
-read -p "enter commit message: " commitmessage
-git add . && git reset config.py && git status && git commit -m '$commitmessage'
