bd098ecf561e2bec0717b7fb472995213b5fe78e
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-04-01 02:26:01 -0400

    'testing'

--- gitscript.py
+++ gitscript.py
@@ -1,6 +1,6 @@
 import os
 
-commit_message = input('commit mesage ')
+commit_message = input('commit mesage: ')
 
 os.system(f"git add . && git reset config.py && git status && git commit -m '{commit_message}' ")
 
