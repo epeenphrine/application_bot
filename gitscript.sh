e71072bf2dfb958cf1e3b3fcb1fbf23bf30bc47b
Dong <56413707+epeenphrine@users.noreply.github.com>
2020-04-01 02:34:53 -0400

    $commitmessage

--- gitscript.sh
+++ gitscript.sh
@@ -1,2 +1,2 @@
 read -p "enter commit message: " commitmessage
-git add . && git reset config.py && git commit -m '$commitmessage'
+git add . && git reset config.py && git status && git commit -m '$commitmessage'
